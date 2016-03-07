f767b91de38f3067819ac23f5fba28add2ecf91a
veeseattle <v-vakurn@microsoft.com>
2016-03-07 13:49:35 -0800

    First commit

---
+++ CSFundamentals/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool leapYear = IsLeapYear(0);
+            Console.WriteLine(leapYear);
+            Console.ReadLine();
+
+        }
+        public static bool IsLeapYear(int year)
+        {
+            if (year <= 0)
+            {
+                Console.WriteLine("Please enter a positive number");
+                Console.ReadLine();
+            }
+            if (year%4 == 0)
+            {
+                if (year%100 != 0)
+                {
+                    return true;
+                }
+                else if (year%400 == 0)
+                {
+                    return true;
+                }
+            }
+            
+                return false;
+            
+        }
+
+
+
+    }
+}
