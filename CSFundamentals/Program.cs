e5638a9cf81c8ea027de00221cd53c0b59f33993
veeseattle <v-vakurn@microsoft.com>
2016-03-07 14:08:18 -0800

    Added first and second part of Lab 1

--- CSFundamentals/Program.cs
+++ CSFundamentals/Program.cs
@@ -10,11 +10,15 @@ namespace CSFundamentals
     {
         static void Main(string[] args)
         {
-            bool leapYear = IsLeapYear(0);
-            Console.WriteLine(leapYear);
+// bool leapYear = IsLeapYear(0);
+// Console.WriteLine(leapYear);
+// Console.ReadLine();
+            DateTime d = new DateTime(2018, 1, 18);
+            bool todayBeforeD = IsBefore(d);
+            Console.WriteLine(todayBeforeD);
             Console.ReadLine();
-
         }
+
         public static bool IsLeapYear(int year)
         {
             if (year <= 0)
@@ -38,7 +42,15 @@ namespace CSFundamentals
             
         }
 
-
+        public static bool IsBefore(DateTime d)
+        {
+            int dateDiff = DateTime.Compare(DateTime.Today, d);
+            if (dateDiff > 0)
+            {
+                return true;
+            }
+            return false;
+        }
 
     }
 }
